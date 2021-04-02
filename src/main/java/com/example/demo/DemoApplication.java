89baf288f59957b4ddabd008eab3fe43876d1441
caothanhhai <caothanhhai430@gmail.com>
2021-04-02 17:19:08 +0700

    read config ini file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println(Config.getParam("dev","a"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
